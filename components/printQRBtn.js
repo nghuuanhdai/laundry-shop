e630cbcaab06544317b489680c788aa7d370dac0
DESKTOP-JLMBUDS\Dai <nnhhaadd@gmail.com>
2022-06-22 22:14:58 +0700

    fix receipt printing function

--- components/printQRBtn.js
+++ components/printQRBtn.js
@@ -2,13 +2,13 @@ export default function PrintQRButton({request}) {
   function printReceipt(evt) {
 		var mywindow = window.open('', 'PRINT', 'height=400,width=600');
 
-    mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+    mywindow.document.write('<html><head><title>' + 'Receipt'  + '</title>');
 		mywindow.document.write('<style> html * {font-family: Arial, Helvetica, sans-serif;} </style>')
     mywindow.document.write('</head><body >');
 
 		mywindow.document.write('<div style="display: flex; justify-content: center; flex-direction: column;   align-items: center;">')
 		mywindow.document.write('<h1>Easy laundry</h1>');
-		mywindow.document.write(`<h2>Receipt: ${request.id}</h2>`);
+		mywindow.document.write(`<h2>Receipt: ${request.id||request._id}</h2>`);
 		mywindow.document.write(`<p>due date: ${request.dueDate}</p>`)
     mywindow.document.write(document.getElementById('qr').innerHTML);
 		mywindow.document.write('</div>')
@@ -18,7 +18,7 @@ export default function PrintQRButton({request}) {
     mywindow.focus(); // necessary for IE >= 10*/
 
     mywindow.print();
-    mywindow.close();
+    // mywindow.close();
 
 	}
 
