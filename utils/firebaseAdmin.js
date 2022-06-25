d67e68819ebf883cb6fe708a68f817075a697448
DESKTOP-JLMBUDS\Dai <nnhhaadd@gmail.com>
2022-06-25 21:04:23 +0700

    fix firebase admin private key

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -7,7 +7,7 @@ if(!global.firebaseAdmin){
         "type":                         process.env.FB_ADMIN_TYPE,
         "project_id":                   process.env.FB_ADMIN_PROJECT_ID,
         "private_key_id":               process.env.FB_ADMIN_PRIVATE_KEY_ID,
-        "private_key":                  (process.env.FB_ADMIN_PRIVATE_KEY).replace('\\n','\n'),
+        "private_key":                  (process.env.FB_ADMIN_PRIVATE_KEY).replace(/\\n/g, '\n'),
         "client_email":                 process.env.FB_ADMIN_CLIENT_EMAIL,
         "client_id":                    process.env.FB_ADMIN_CLIENT_ID,
         "auth_uri":                     process.env.FB_ADMIN_AUTH_URI,
